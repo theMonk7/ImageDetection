72879ef5111f3f879302d3584980337c0e2da505
utkarshraj <utkarshraj@urbancompany.com>
2024-11-01 18:17:52 +0530

    Initial commit

---
+++ ImageDetection/ImageDetection/ImageDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDetectionApp.swift
+//  ImageDetection
+//
+//  Created by Utkarsh Raj on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
